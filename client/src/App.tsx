588f8ff7a52d3f09cd1a8e09a9e208326d74e2e4
Basil Al_Sheikh <basilelshakh@gmail.com>
2025-03-26 10:33:46 +0200

    implement not found page. relates #12

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "./components/ui/Toaster";
+import NotFoundPage from "./pages/NotFoundPage";
 
 /**
  * Main App component
@@ -40,6 +41,7 @@ const App = () => {
           <Route path="/auth/login" element={<LoginPage />} />
           <Route path="/auth/register" element={<RegisterPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Layout>
       <Toaster />
